5bce515ed326875a79af88c17423abe76b35c7f2
Zarina Nazimidinova <zarinanazimidinova229@gmail.com>
2023-10-21 16:56:24 +0600

    первая фиксация

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById('redButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('greenButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('blueButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
